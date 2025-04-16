1421cfd6e91daf8ce3903da7df529e7733f3a277
James Ratcliff <falazar@yahoo.com>
2025-04-16 14:32:00 -0500

    Cleanups.

--- src/main/java/com/falazar/farmupcraft/FarmUpCraft.java
+++ src/main/java/com/falazar/farmupcraft/FarmUpCraft.java
@@ -96,6 +96,7 @@ public class FarmUpCraft {
         ShowBiomesCommand.register(e.getDispatcher());
         ManagersCommand.register(e.getDispatcher());
         DataBaseCommand.register(e.getDispatcher());
+        PlayerCommand.register(e.getDispatcher());
         PlotCommand.register(e.getDispatcher());
         VillageCommand.register(e.getDispatcher());
     }
